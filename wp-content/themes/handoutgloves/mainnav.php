c5af70e0a527470971858a00a2ca647bcef6f621
Trevor <trevorhoward2@hotmail.com>
2017-10-22 16:14:51 -0600

    Fixed footer

--- wp-content/themes/handoutgloves/mainnav.php
+++ wp-content/themes/handoutgloves/mainnav.php
@@ -4,16 +4,16 @@
   </button>
   <div class="collapse navbar-collapse div-class" id="navbarSupportedContent">
     <ul class="navbar-nav custom-class">
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
       <li class="nav-item">
         <button class="nav-link btn btn-md btn-primary" href="#">Login <span class="sr-only">(current)</span></button>
       </li>
       <li class="nav-item">
         <button class="nav-link btn btn-md btn-primary" href="#">Cart</button>
       </li>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
     </ul>
   </div>
 </nav>
