ca3df59143b098079dd39f1b6f547749dd40e2e0
Cody Higgins <cody.higginsdev18@gmail.com>
2017-10-22 18:18:28 -0600

    continued home page styles and content

--- wp-content/themes/handoutgloves/page-home.php
+++ wp-content/themes/handoutgloves/page-home.php
@@ -10,47 +10,76 @@
   </div>
 
 
-  <div class="row home-page-img-style">
-    <div class="col-4">
-      <img src="http://via.placeholder.com/175x250">
-    </div>
-
-    <div class="col-4">
-      <img src="http://via.placeholder.com/175x250">
-    </div>
-
-    <div class="col-4">
-      <img src="http://via.placeholder.com/175x250">
-    </div>
+  <div class="row justify-content-center">
+    <img src="http://via.placeholder.com/1000x400">
   </div>
 
-  <div class="row justify-content-center">
+  <div class="row justify-content-center buy-now-btn-style">
     <button class="btn btn-lg btn-primary btn-styles">Buy Now</button>
   </div>
 
 
   <div class="main-content-style">
     <div class="upper-case-top-home-screen">
-      <h4>What do you do?</h4>
+      <h2>What do you do?</h2>
     </div>
   </div>
 
 
   <div class="row home-page-img-style">
     <div class="col-3">
-      <img src="http://via.placeholder.com/175x250">
+      <img src="<?php echo theme_image('HO Snow5.png') ?>" style="width:65%">
     </div>
     <div class="col-3">
-      <img src="http://via.placeholder.com/175x250">
+      <img src="<?php echo theme_image('HO Fish5.png') ?>" style="width:65%">
     </div>
     <div class="col-3">
-      <img src="http://via.placeholder.com/175x250">
+      <img src="<?php echo theme_image('HO Hunt5.png') ?>" style="width:65%">
     </div>
     <div class="col-3">
-      <img src="http://via.placeholder.com/175x250">
+      <img src="<?php echo theme_image('HO Work5.png') ?>" style="width:65%">
     </div>
   </div>
+
+  <div class="row justify-content-center main-glove-homepage">
+    <img src="http://via.placeholder.com/1000x400">
+  </div>
+
+  <div class="main-content-style">
+    <div class="upper-case-top-home-screen">
+      <h3>The solution is here</h3>
+    </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque lectus, commodo in nullaeget, sodales dictum magna. Vivamus justo nulla, efficitur id rhoncus eu, aliquet sit amet mi.</p>
+      <div class="row justify-content-center">
+        <button class="btn btn-lg btn-primary btn-styles1">Learn More</button>
+      </div>
+  </div>
+
+  <div class="whole-sale-page-break">
+    <div class="row justify-content-center">
+      <div class="vertical-align-wholesale">
+        <h1 class="whole-sale-page-break1">Yo Whole Sale Venders</h1>
+        <div class="row justify-content-center">
+          <a href="#" class="whole-sale-button">Click Here</a>
+        </div>
+      </div>
+    </div> 
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
 </div>
+
+
 
 
 
