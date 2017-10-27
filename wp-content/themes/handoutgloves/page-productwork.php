baece6123573a2cb8d05418e751e9cf22860f5a3
Cody Higgins <cody.higginsdev18@gmail.com>
2017-10-26 20:44:31 -0600

    mostly completed home, and product landing pages

--- wp-content/themes/handoutgloves/page-productwork.php
+++ wp-content/themes/handoutgloves/page-productwork.php
@@ -1,71 +1,99 @@
 <div class="home">
-	<div class="row">
-		<div class="col-4 snow-productpage-header">
-			<h1>WORK GLOVE</h1>
-			<div class="snow-product-first-paragraph1">	
-				<p>Lorem ipsum dolor sit amet, consectetur</p>
-				<p>adipiscing elit. Donec neque lectus,</p>
-				<p>commodo in nulla eget, dictum magna.</p>
-			</div>
-			<div class="snow-product-first-paragraph1">	
-				<p>Lorem ipsum dolor sit amet, consectetur</p>
-				<p>adipiscing elit. Donec neque lectus,</p>
-				<p>commodo in nulla eget, dictum magna.</p>
-			</div>
-			<div class="form-group">
-    		<select id="exampleSelect1" class="model-btn" placeholder="Model">
-		      <option>Model</option>
-		      <option>Pro</option>
-		      <option>Sport</option>
-		      <option>Light Weight</option>
-		    </select>
-	    	<input type="Quantity" class="quantity-btn" id="exampleInputPassword1" placeholder="QTY">
-		  </div>
-		  <div class="form-group">
-			  <div class="row justify-content-center">
-	    		<input type="size" class="size-buttons" id="exampleInputPassword1" placeholder="XS">
-	    		<input type="size" class="size-buttons" id="exampleInputPassword1" placeholder="S">
-	    		<input type="size" class="size-buttons" id="exampleInputPassword1" placeholder="M">
-	    		<input type="size" class="size-buttons" id="exampleInputPassword1" placeholder="L">
-	    		<input type="size" class="size-buttons" id="exampleInputPassword1" placeholder="XL">
+	<div class="product-landing-page-container">
+		<div class="row">
+			<div class="col-4 snow-productpage-header" style="background:url('<?php echo theme_image('HO Work6.png'); ?>') no-repeat center center;background-size: 90% 90%;">
+				<h1>WORK GLOVE</h1>
+				<div class="snow-product-first-paragraph1">	
+					<p>Lorem ipsum dolor sit amet, consectetur</p>
+					<p>adipiscing elit. Donec neque lectus,</p>
+					<p>commodo in nulla eget, dictum magna.</p>
+				</div>
+				<div class="snow-product-first-paragraph2">	
+					<p>Lorem ipsum dolor sit amet, consectetur</p>
+					<p>adipiscing elit. Donec neque lectus,</p>
+					<p>commodo in nulla eget, dictum magna.</p>
+				</div>
+				<div class="form-group">
+	    		<select id="exampleSelect1" class="model-btn" placeholder="Model">
+			      <option>Model</option>
+			      <option>Pro</option>
+			      <option>Sport</option>
+			      <option>Light Weight</option>
+			    </select>
+		    	<input type="Quantity" class="quantity-btn" id="exampleInputPassword1" placeholder="QTY">
+			  </div>
+			  <div class="form-group">
+				  <div class="row justify-content-center">
+		    		<button type="size" class="size-buttons" id="exampleInputPassword1" placeholder="XS">XS</button>
+		    		<button type="size" class="size-buttons" id="exampleInputPassword1" placeholder="S">S</button>
+		    		<button type="size" class="size-buttons" id="exampleInputPassword1" placeholder="M">M</button>
+		    		<button type="size" class="size-buttons" id="exampleInputPassword1" placeholder="L">L</button>
+		    		<button type="size" class="size-buttons" id="exampleInputPassword1" placeholder="XL">XL</button>
+		  		</div>
+		  	</div>
+	  		<div class="form-group">
+	  			<button type="cart" class="cart-button" id="exampleInputPassword1" placeholder="ADD TO CART">ADD TO CART</button>
 	  		</div>
-	  	</div>
-  		<div class="form-group">
-  			<input type="password" class="cart-button" id="exampleInputPassword1" placeholder="ADD TO CART">
-  		</div>
+			</div>
+
+			<div class="col-8">
+
+			</div>
 		</div>
 
-		<div class="col-8">
-
-		</div>
-	</div>
-
-	<div class="row">
-		<div class="col-4 product-page-bottom">
-			<div class="row">
-				<img src="http://via.placeholder.com/100x200">	
+		<div class="row product-page-bottom-content">
+			<div class="col-1">
+				<div class="product-page-bottom-pictures">
+					<img src="http://via.placeholder.com/100x200">
+				</div>
+			</div>
+			<div class="col-2">
 				<h4>Pro</h4>
 				<a href="#">Learn More</a>
+				<br>
+				<a href="#">Buy Now</a>
+			</div>
+
+
+			<div class="col-1 product-page-bottom-pictures">
+				<div class="product-page-bottom-pictures">
+					<img src="http://via.placeholder.com/100x200">
+				</div>
+			</div>
+			<div class="col-2">
+				<h4>Sport</h4>
+				<a href="#">Learn More</a>
+				<br>
+				<a href="#">Buy Now</a>
+			</div>
+
+
+			<div class="col-1 product-page-bottom-pictures">
+				<div class="product-page-bottom-pictures">
+					<img src="http://via.placeholder.com/100x200">
+				</div>
+			</div>
+			<div class="col-2">
+				<h4>Light Weight</h4>
+				<a href="#">Learn More</a>
+				<br>
+				<a href="#">Buy Now</a>
+			</div>
+
+			<div class="col-1 product-page-bottom-pictures">
+				<div class="product-page-bottom-pictures">
+					<img src="http://via.placeholder.com/100x200">
+				</div>
+			</div>
+			<div class="col-2">
+				<h4>Childrens</h4>
+				<a href="#">Learn More</a>
+				<br>
 				<a href="#">Buy Now</a>
 			</div>
 		</div>
-
-			<div class="col-4">
-				<div class="row">
-					<img src="http://via.placeholder.com/100x200">
-					<h4>Sport</h4>
-					<a href="#">Learn More</a>
-					<a href="#">Buy Now</a>
-				</div>
-			</div>
-
-			<div class="col-4">
-				<div class="row">
-					<img src="http://via.placeholder.com/100x200">
-					<h4>Light Weight</h4>
-					<a href="#">Learn More</a>
-					<a href="#">Buy Now</a>
-				</div>
-			</div>
-		</div>
-</div>	
+	</div>
+</div>
+	<div class="snow-landing-footer">
+    <img src="<?php echo theme_image('work-landing-footer-1.png'); ?>" class="landing-page-footer">
+  </div>
