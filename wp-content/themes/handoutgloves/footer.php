fb0955c1c66de92d82484dea75bf77625d7b07a1
Trevor <trevorhoward2@hotmail.com>
2017-10-28 18:29:40 -0600

    mobile footer fixes

--- wp-content/themes/handoutgloves/footer.php
+++ wp-content/themes/handoutgloves/footer.php
@@ -2,7 +2,7 @@
       <footer>
         <div class="container">
           <div class="row footer-text-styles">
-            <div class="col-4">
+            <div class="col-12 col-lg-4">
               <p class="underline">social<p>
               <div class="container">
                 <div class="row">
@@ -23,7 +23,7 @@
               <hr class="text-white">
               <img src="<?php echo theme_image('HO Full2 (White).png') ?>" style="width:100px; margin:0 auto; display: block">
             </div>
-            <div class="col-4">
+            <div class="col-12 col-lg-4">
               <p class="underline">products</p>
               <p><a href="#">snow</a></p>
               <p><a href="#">hunt</a></p>
@@ -31,7 +31,7 @@
               <p><a href="#">work</a></p>
               <p><a href="#">clothing</a></p>
             </div>
-            <div class="col-4">
+            <div class="col-12 col-lg-4">
               <p class="underline">resources</p>
               <p><a href="#">wholesale accounts</a></p>
               <p><a href="#">support</a></p>
